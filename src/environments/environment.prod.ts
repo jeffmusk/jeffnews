3ff0980f94add0f8f659087fa910a0e4a5a6a149
Jeff <jeffprogramacion@gmail.com>
2018-05-18 23:29:42 -0500

    version 2 firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  	apiKey: "AIzaSyBc3J4WOeSZJzxW8MZ482EgL3KFNQ-rWPg",
+    authDomain: "jeffmeganews.firebaseapp.com",
+    databaseURL: "https://jeffmeganews.firebaseio.com",
+    projectId: "jeffmeganews",
+    storageBucket: "",
+    messagingSenderId: "139932751734"
+  },
+  apiKey: "0c60ca0732284356bbccab4752ae45fa"
 };
